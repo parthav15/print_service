e2666eeeff4fa792ba9d9fb27066c45db1283336
parthav15 <parthavsabrwal@gmail.com>
2024-10-01 00:03:45 +0530

    ADDED REQUIREMENTS.TXT

--- print_service/settings.py
+++ print_service/settings.py
@@ -136,7 +136,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -159,12 +159,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # SECRET_KEY = eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcyNTc3OTY4MiwiaWF0IjoxNzI1Nzc5NjgyfQ.idCYYpJ0mx0JwQpL3PFecd1rjiAMiQ_60Z4HqSVSPvs
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtppro.zoho.in'
+# EMAIL_PORT = 465
+# EMAIL_HOST_USER = 'noreply@ntftravel.com'
+# EMAIL_HOST_PASSWORD = 'NtfTravel@421#'
+# EMAIL_USE_SSL = True
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtppro.zoho.in'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'noreply@ntftravel.com'
-EMAIL_HOST_PASSWORD = 'NtfTravel@421#'
-EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'parthavsabrwal@gmail.com'
+EMAIL_HOST_PASSWORD = 'jynr wrfr rzsx esej'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 
 RAZORPAY_KEY_ID = 'rzp_test_ew74Ktx27rLLPC'
 RAZORPAY_KEY_SECRET = 'zM9aUHrCKXRyoyy6DYXNjzda'
